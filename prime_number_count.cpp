a5ce2274ed1908e732560bb4f05288e447d9c8f4
MABelote <mbelote@cub.uca.edu>
2025-04-19 13:17:01 -0500

    Update prime_number_count.cpp

--- prime_number_count.cpp
+++ prime_number_count.cpp
@@ -1,6 +1,8 @@
 /*-- prime_count_serial.cpp-----------------------------------------------------------
    This file implements a program that fills an arry with numbers and 
    then counts the prime numbers in the array
+
+   Student: Alex Belote
 -------------------------------------------------------------------------*/
 
 #include <iostream>
@@ -44,6 +46,21 @@ int count_prime_serial(long int numbers[], long int how_many)
 	return count;
 }
 
+
+int count_prime_numbers(long int numbers[], long int how_many) {
+	int count = 0;
+#pragma omp parallel for reduction(+:count)
+	for (int i = 0; i < how_many; i++) {
+		if (is_prime(numbers[i])) {
+			count++;
+		}
+	}
+
+	return count;
+
+
+}
+
 // This is the entrypoint into the program
 int main() {
 	long int n = 1000000;
@@ -58,6 +75,17 @@ int main() {
 	auto start = chrono::steady_clock::now();
 	int count = count_prime_serial(numbers, n);
 	auto end = chrono::steady_clock::now();
+
+	cout << "Counting primes (parallel proccessing)..." << endl;
+	auto start_parallel = chrono::steady_clock::now();
+	int count_parallel = count_prime_numbers(numbers, n);
+	auto end_parallel = chrono::steady_clock::now();
+
+	double compute_time_parallel = chrono::duration<double>(end_parallel - start_parallel).count();
+
+	cout << "Total number of primes (parallel) = " << count_parallel << endl;
+	cout << "Total computation time (parallel) = " << compute_time_parallel << " seconds" << endl;
+
 
 	// Print results
 	double compute_time = chrono::duration<double>(end - start).count();
